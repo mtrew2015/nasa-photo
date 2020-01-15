0b1fbbf6eccd6181ff96c81f68acdabad43ac8d5
Michael Trew <mtrew2015@gmail.com>
2020-01-15 03:00:38 -0500

    styles

--- src/components/Mars/Mars.js
+++ src/components/Mars/Mars.js
@@ -31,13 +31,12 @@ function Mars() {
 
 	return (
         <div className="marsWrapper">
-        {isTrue? <h1>Yes its true</h1>: ''}
-        <h2>Mars Weather Last 5 Days</h2>
+        <h2>Recent Weather On Mars</h2>
 			<div className='marsWeather'>
 				
 				{data.slice(1, 6).map((day,idx) => (
 					<div className="day" key={idx}>
-						<p><span>Date:</span>{day.First_UTC.slice(0, 10)}</p>
+						<p><span>Date:</span>{day.First_UTC.slice(5, 10)}</p>
 						<p><span>Average</span>{day.AT.av.toFixed(0)}&deg;</p>
 
 						<p><span>High:</span>{day.AT.mx.toFixed(0) }&deg;</p>
@@ -47,7 +46,7 @@ function Mars() {
 					</div>
 				))}
             </div>
-            <h2>Mars Rover Photos</h2>
+            <h2 className="rover">Mars Rover Photos</h2>
             <div className="photo-container">
             
             {console.log(pictures, 'pictures.photos')}
